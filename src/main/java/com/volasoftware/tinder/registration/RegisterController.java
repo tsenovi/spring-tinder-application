3d0ba930db7b18f8457bcd462dc2fccfe1f0496d
tsenovi <vandertsen@gmail.com>
2023-02-11 14:59:22 +0200

    Documented RegisterController

--- src/main/java/com/volasoftware/tinder/registration/RegisterController.java
+++ src/main/java/com/volasoftware/tinder/registration/RegisterController.java
@@ -3,23 +3,31 @@ package com.volasoftware.tinder.registration;
 import com.volasoftware.tinder.accounts.Account;
 import com.volasoftware.tinder.dto.RegisterDTO;
 import com.volasoftware.tinder.accounts.AccountService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@RequestMapping("/api/v1/users")
+@Api(value = "Register controller")
 public class RegisterController {
 
   private final AccountService accountService;
 
-  @PostMapping(value = "/api/v1/users/register",
-      consumes = {"application/xml","application/json"})
+  @ApiOperation(value = "Register new account")
+  @PostMapping(value = "/register",
+      consumes = {"application/xml", "application/json"})
   public ResponseEntity<Account> registerNewAccount(
+      @ApiParam(value = "Registration details", required = true)
       @RequestBody RegisterDTO registerDTO) {
 
     return new ResponseEntity<>(accountService.save(registerDTO), HttpStatus.CREATED);
