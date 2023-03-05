e6f964db071944b1d4f4633e5815767b764c2c66
tsenovi <vandertsen@gmail.com>
2023-03-05 09:46:48 +0200

    Added swagger docs for responses

--- src/main/java/com/volasoftware/tinder/dtos/ErrorResponseDTO.java
+++ src/main/java/com/volasoftware/tinder/dtos/ErrorResponseDTO.java
@@ -1,6 +1,8 @@
 package com.volasoftware.tinder.dtos;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,10 +14,16 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@ApiModel(description = "Error response information")
 public class ErrorResponseDTO {
 
+  @ApiModelProperty(value = "Http status code", example = "404")
   private HttpStatus status;
+
+  @ApiModelProperty(value = "Error message", example = "Not Found")
   private String message;
+
+  @ApiModelProperty(value = "Timestamp of the error", example = "2021-15-08 14:32:17")
   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
   private LocalDateTime timestamp;
 
