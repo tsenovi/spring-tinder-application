dd8be3ed4d8b3c0a3a770fcd9adc792b4f9b412c
tsenovi <vandertsen@gmail.com>
2023-11-09 22:22:25 +0200

    fixed constants

--- src/test/java/com/volasoftware/tinder/repositories/AccountRepositoryTest.java
+++ src/test/java/com/volasoftware/tinder/repositories/AccountRepositoryTest.java
@@ -16,18 +16,18 @@ public class AccountRepositoryTest {
 
   @Autowired
   private AccountRepository accountRepository;
-  private final String firstName = "Test";
-  private final String email = "Test_Test@gmail.com";
-  private final Long id = 1L;
+  private static final String FIRST_NAME = "Test";
+  private static final String EMAIL = "Test_Test@gmail.com";
+  private static final Long ID = 1L;
 
   @BeforeEach
   void setUp() {
     // create a test entity
     Account account = new Account();
-    account.setId(id);
-    account.setFirstName(firstName);
+    account.setId(ID);
+    account.setFirstName(FIRST_NAME);
     account.setLastName("Test");
-    account.setEmail(email);
+    account.setEmail(EMAIL);
     account.setPassword("password");
     account.setCreatedDate(new Date());
     account.setLastModifiedDate(new Date());
@@ -40,11 +40,11 @@ public class AccountRepositoryTest {
   @Test
   public void testFindSavedAccountWhenGivenTheIdThenAssertDataMatches() {
     // find the entity by ID
-    Optional<Account> result = accountRepository.findById(id);
+    Optional<Account> result = accountRepository.findById(ID);
 
     // verify that the entity was found
     assertThat(result.isPresent()).isTrue();
-    assertThat(result.get().getFirstName()).isEqualTo(firstName);
-    assertThat(result.get().getEmail()).isEqualTo(email);
+    assertThat(result.get().getFirstName()).isEqualTo(FIRST_NAME);
+    assertThat(result.get().getEmail()).isEqualTo(EMAIL);
   }
 }
