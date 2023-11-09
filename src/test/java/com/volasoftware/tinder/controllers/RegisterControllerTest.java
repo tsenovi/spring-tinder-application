4eb42f12e7f8a420f050f1a2704590a0cc3b113a
tsenovi <vandertsen@gmail.com>
2023-11-09 21:52:23 +0200

    refactoring

--- src/test/java/com/volasoftware/tinder/controllers/RegisterControllerTest.java
+++ src/test/java/com/volasoftware/tinder/controllers/RegisterControllerTest.java
@@ -6,7 +6,6 @@ import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,15 +15,13 @@ import com.volasoftware.tinder.dtos.RegisterDTO;
 import com.volasoftware.tinder.services.contracts.AccountService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(RegisterController.class)
-public class RegistrationControllerIntegrationTests {
+public class RegisterControllerTest {
 
   @Autowired
   private RegisterController registerController;
@@ -34,11 +31,6 @@ public class RegistrationControllerIntegrationTests {
 
   @MockBean
   private AccountService accountService;
-
-  @Test
-  void contextLoads() {
-    assertThat(registerController).isNotNull();
-  }
 
   @Test
   void givenRegistrationDetailsWhenEmailNotExistThenCreatedAccount() throws Exception {
--- src/test/java/com/volasoftware/tinder/repositories/AccountRepositoryTest.java
+++ src/test/java/com/volasoftware/tinder/repositories/AccountRepositoryTest.java
@@ -1,10 +1,9 @@
-package com.volasoftware.tinder;
+package com.volasoftware.tinder.repositories;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import com.volasoftware.tinder.constants.Gender;
 import com.volasoftware.tinder.models.Account;
-import com.volasoftware.tinder.repositories.AccountRepository;
 import java.util.Date;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,19 +12,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
-public class H2DatabaseTest {
+public class AccountRepositoryTest {
 
   @Autowired
   private AccountRepository accountRepository;
+  private final String firstName = "Test";
+  private final String email = "Test_Test@gmail.com";
+  private final Long id = 1L;
 
   @BeforeEach
   void setUp() {
     // create a test entity
     Account account = new Account();
-    account.setId(1L);
-    account.setFirstName("Test");
+    account.setId(id);
+    account.setFirstName(firstName);
     account.setLastName("Test");
-    account.setEmail("Test_Test@gmail.com");
+    account.setEmail(email);
     account.setPassword("password");
     account.setCreatedDate(new Date());
     account.setLastModifiedDate(new Date());
@@ -33,18 +35,16 @@ public class H2DatabaseTest {
 
     // save to repository
     accountRepository.saveAndFlush(account);
-    System.out.println("Number of accounts in the database: " + account.getEmail());
   }
 
   @Test
   public void testFindSavedAccountWhenGivenTheIdThenAssertDataMatches() {
     // find the entity by ID
-    Optional<Account> result = accountRepository.findById(1L);
+    Optional<Account> result = accountRepository.findById(id);
 
     // verify that the entity was found
     assertThat(result.isPresent()).isTrue();
-    assertThat(result.get().getFirstName()).isEqualTo("Test");
-    assertThat(result.get().getEmail()).isEqualTo("Test_Test@gmail.com");
+    assertThat(result.get().getFirstName()).isEqualTo(firstName);
+    assertThat(result.get().getEmail()).isEqualTo(email);
   }
-
 }
