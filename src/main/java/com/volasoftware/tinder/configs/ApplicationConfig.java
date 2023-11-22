d3133f75272bfa7afea288c397ea4121f5f1b581
tsenovi <vandertsen@gmail.com>
2023-11-22 16:52:50 +0200

    Created beans of authentication Manager and Provider

--- src/main/java/com/volasoftware/tinder/configs/ApplicationConfig.java
+++ src/main/java/com/volasoftware/tinder/configs/ApplicationConfig.java
@@ -5,7 +5,13 @@ import com.volasoftware.tinder.repositories.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
@@ -19,6 +25,25 @@ public class ApplicationConfig {
   public UserDetailsService userDetailsService() {
     return userName -> accountRepository.findOneByEmail(userName)
         .orElseThrow(AccountNotFoundException::new);
+  }
+
+  @Bean
+  public AuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    authProvider.setUserDetailsService(userDetailsService());
+    authProvider.setPasswordEncoder(passwordEncoder());
+    return authProvider;
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
+      throws Exception {
+    return config.getAuthenticationManager();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 
 }
