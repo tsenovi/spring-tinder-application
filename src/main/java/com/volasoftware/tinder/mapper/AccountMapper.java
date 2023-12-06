27e093b8a0cf4883335f955f7f7e5ce78a75c0bd
tsenovi <vandertsen@gmail.com>
2023-12-06 15:38:22 +0200

    Added back the unmappedPolicy attribute

--- src/main/java/com/volasoftware/tinder/mapper/AccountMapper.java
+++ src/main/java/com/volasoftware/tinder/mapper/AccountMapper.java
@@ -6,8 +6,9 @@ import com.volasoftware.tinder.dtos.RegisterRequest;
 import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface AccountMapper {
 
   @Mapping(target = "createdDate", ignore = true)
