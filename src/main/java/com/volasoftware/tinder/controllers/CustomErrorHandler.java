7ded0d0b7378330e4be91bda93f9ea7820482059
tsenovi <vandertsen@gmail.com>
2023-12-09 13:38:54 +0200

    Optimized CustomErrorHandler

--- src/main/java/com/volasoftware/tinder/controllers/CustomErrorHandler.java
+++ src/main/java/com/volasoftware/tinder/controllers/CustomErrorHandler.java
@@ -2,13 +2,16 @@ package com.volasoftware.tinder.controllers;
 
 import com.volasoftware.tinder.exceptions.*;
 import com.volasoftware.tinder.responses.ResponseHandler;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
@@ -20,39 +23,28 @@ public class CustomErrorHandler extends ResponseEntityExceptionHandler {
         return ResponseHandler.generateResponse(exception.getMessage(), HttpStatus.NOT_FOUND, null);
     }
 
-    @ExceptionHandler({EmailIsTakenException.class})
+    @ExceptionHandler({
+            EmailIsTakenException.class,
+            AccountNotVerifiedException.class,
+            VerificationTokenExpiredException.class
+    })
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<?> handleBadRequestException(RuntimeException exception) {
         return ResponseHandler.generateResponse(exception.getMessage(), HttpStatus.BAD_REQUEST, null);
     }
 
-    @ExceptionHandler({EmailIsNotValidException.class})
+    @ExceptionHandler({
+            EmailIsNotValidException.class,
+            EmailAlreadyVerifiedException.class
+    })
     @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
     public ResponseEntity<?> handleNotAcceptableException(RuntimeException exception) {
         return ResponseHandler.generateResponse(exception.getMessage(), HttpStatus.NOT_ACCEPTABLE, null);
     }
 
-    @ExceptionHandler({EmailAlreadyVerifiedException.class})
-    @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
-    public ResponseEntity<?> handleEmailAlreadyVerifiedException(RuntimeException exception) {
-        return ResponseHandler.generateResponse(exception.getMessage(), HttpStatus.NOT_ACCEPTABLE, null);
-    }
-
-    @ExceptionHandler({VerificationTokenExpiredException.class})
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<?> handleVerificationTokenExpiredException(RuntimeException exception) {
-        return ResponseHandler.generateResponse(exception.getMessage(), HttpStatus.BAD_REQUEST, null);
-    }
-
-    @ExceptionHandler({AccountNotVerifiedException.class})
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<?> handleAccountNotVerifiedException(RuntimeException exception) {
-        return ResponseHandler.generateResponse(exception.getMessage(), HttpStatus.BAD_REQUEST, null);
-    }
-
     @ExceptionHandler({AuthenticationException.class})
     @ResponseStatus(HttpStatus.FORBIDDEN)
-    public ResponseEntity<?> handleAuthenticationException(AuthenticationException exception) {
+    public ResponseEntity<?> handleAuthenticationException(RuntimeException exception) {
         return ResponseHandler.generateResponse(exception.getMessage(), HttpStatus.FORBIDDEN, null);
     }
 }
