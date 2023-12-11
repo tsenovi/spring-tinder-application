3985a02aabf79e672d53544fb7fa4f8821e5f819
tsenovi <vandertsen@gmail.com>
2023-12-11 12:58:52 +0200

    added authController.login test

--- src/test/java/com/volasoftware/tinder/controllers/AuthenticationControllerTest.java
+++ src/test/java/com/volasoftware/tinder/controllers/AuthenticationControllerTest.java
@@ -2,19 +2,24 @@ package com.volasoftware.tinder.controllers;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.volasoftware.tinder.constants.AccountConstant;
 import com.volasoftware.tinder.constants.Gender;
 import com.volasoftware.tinder.dtos.AccountDto;
+import com.volasoftware.tinder.dtos.LoginRequest;
 import com.volasoftware.tinder.dtos.RegisterRequest;
+import com.volasoftware.tinder.models.Account;
+import com.volasoftware.tinder.responses.LoginResponse;
 import com.volasoftware.tinder.services.contracts.AuthenticationService;
+import com.volasoftware.tinder.services.contracts.JwtService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -23,14 +28,35 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.time.LocalDateTime;
+import java.time.Month;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AuthenticationControllerTest {
 
     private static final String REGISTER_URI = "/api/v1/users/register";
+    private static final String LOGIN_URI = "/api/v1/users/login";
+
+    public static final LocalDateTime LOCAL_DATE_TIME = LocalDateTime.of(
+            2023,
+            Month.DECEMBER,
+            7,
+            12,
+            30,
+            00,
+            50000);
+    private static final String FIRST_NAME = "Test";
+    private static final String EMAIL = "Test_Test@gmail.com";
+    private static final Long ID = 1L;
+    private static final String LAST_NAME = "Test";
+    private static final String PASSWORD = "password";
+
     private MockMvc mockMvc;
     @MockBean
     private AuthenticationService authenticationService;
+    @Autowired
+    private JwtService jwtService;
 
     @BeforeEach
     public void setUp(WebApplicationContext webApplicationContext) {
@@ -66,6 +92,41 @@ public class AuthenticationControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.body.firstName").value("John"))
                 .andExpect(jsonPath("$.body.email").value("john@gmail.com"));
+    }
+
+    @Test
+    void testLoginWhenAccountVerifiedThenSuccessfulOperation() throws Exception {
+
+        //Given
+        LoginRequest loginRequest = new LoginRequest(EMAIL, PASSWORD);
+        Account account = generateAccount();
+        String jwtToken = jwtService.generateToken(account);
+        account.setVerified(true);
+        LoginResponse loginResponse = new LoginResponse(jwtToken);
+
+        // When
+        given(authenticationService.login(loginRequest)).willReturn(loginResponse);
+
+        // Then
+        mockMvc
+                .perform(post(LOGIN_URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(loginRequest)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value(AccountConstant.LOGGED_IN));
+    }
+
+    private Account generateAccount() {
+        Account account = new Account();
+        account.setId(ID);
+        account.setFirstName(FIRST_NAME);
+        account.setLastName(LAST_NAME);
+        account.setEmail(EMAIL);
+        account.setPassword(PASSWORD);
+        account.setCreatedDate(LOCAL_DATE_TIME);
+        account.setLastModifiedDate(LOCAL_DATE_TIME);
+        account.setGender(Gender.MALE);
+        return account;
     }
 
     private static String asJsonString(final Object obj) {
