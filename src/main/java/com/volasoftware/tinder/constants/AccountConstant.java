f777aaec4c1ec5c69b4426bb2f526d58896228d9
tsenovi <vandertsen@gmail.com>
2023-12-13 12:40:32 +0200

    Created PUT method in the AuthController, added new AccountConstant

--- src/main/java/com/volasoftware/tinder/constants/AccountConstant.java
+++ src/main/java/com/volasoftware/tinder/constants/AccountConstant.java
@@ -2,11 +2,12 @@ package com.volasoftware.tinder.constants;
 
 public class AccountConstant {
 
-    public static final String NOT_FOUND = "Account not found!";
-    public static final String VERIFIED = "Account verified!";
-    public static final String REGISTERED = "Account registered!";
-    public static final String LOGGED_IN = "Account logged in!";
-    public static final String WRONG_PASSWORD = "Wrong password";
-    public static final String NOT_VERIFIED = "Account not verified!";
-    public static final String DETAILS = "Account details!";
+  public static final String NOT_FOUND = "Account not found!";
+  public static final String VERIFIED = "Account verified!";
+  public static final String REGISTERED = "Account registered!";
+  public static final String LOGGED_IN = "Account logged in!";
+  public static final String WRONG_PASSWORD = "Wrong password";
+  public static final String NOT_VERIFIED = "Account not verified!";
+  public static final String DETAILS = "Account details!";
+  public static final String UPDATED = "Account updated!";
 }
