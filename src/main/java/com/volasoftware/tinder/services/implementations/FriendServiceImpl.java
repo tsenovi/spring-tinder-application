c5c4bb624c63e619d0bd76fe7549d38552ad0797
tsenovi <vandertsen@gmail.com>
2024-01-03 13:39:39 +0200

    Added checks and exceptions for friend status

--- src/main/java/com/volasoftware/tinder/services/implementations/FriendServiceImpl.java
+++ src/main/java/com/volasoftware/tinder/services/implementations/FriendServiceImpl.java
@@ -2,9 +2,12 @@ package com.volasoftware.tinder.services.implementations;
 
 import com.volasoftware.tinder.constants.AccountConstant;
 import com.volasoftware.tinder.exceptions.AccountNotFoundException;
+import com.volasoftware.tinder.exceptions.FriendExistException;
+import com.volasoftware.tinder.exceptions.FriendNotFoundException;
 import com.volasoftware.tinder.models.Account;
 import com.volasoftware.tinder.repositories.AccountRepository;
 import com.volasoftware.tinder.services.contracts.FriendService;
+import java.util.HashSet;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -21,7 +24,15 @@ public class FriendServiceImpl implements FriendService {
         Account loggedAccount = getLoggedAccount();
         Account friendAccount = getAccountById(friendId);
 
-        loggedAccount.getFriends().add(friendAccount);
+        if (hasFriends(loggedAccount)) {
+            if (friendExist(loggedAccount, friendAccount)) {
+                throw new FriendExistException(AccountConstant.ALREADY_EXIST);
+            }
+        }
+
+        HashSet<Account> friends = new HashSet<>();
+        friends.add(friendAccount);
+        loggedAccount.setFriends(friends);
         accountRepository.save(loggedAccount);
     }
 
@@ -30,8 +41,20 @@ public class FriendServiceImpl implements FriendService {
         Account loggedAccount = getLoggedAccount();
         Account friendAccount = getAccountById(friendId);
 
-        loggedAccount.getFriends().remove(friendAccount);
-        accountRepository.save(loggedAccount);
+        if (!hasFriends(loggedAccount)) {
+            throw new FriendNotFoundException(AccountConstant.NOT_FOUND);
+        } else if (friendExist(loggedAccount, friendAccount)) {
+            loggedAccount.getFriends().remove(friendAccount);
+            accountRepository.save(loggedAccount);
+        }
+    }
+
+    private boolean hasFriends(Account loggedAccount) {
+        return loggedAccount.getFriends() != null;
+    }
+
+    private boolean friendExist(Account loggedAccount, Account friendAccount) {
+        return loggedAccount.getFriends().contains(friendAccount);
     }
 
     private Account getAccountById(Long id) {
