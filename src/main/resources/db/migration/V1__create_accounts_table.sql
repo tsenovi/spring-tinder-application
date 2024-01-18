e3a595205693904a82f0f0281e1d9864ceb77f13
tsenovi <vandertsen@gmail.com>
2024-01-18 12:24:13 +0200

    unified sql syntax

--- src/main/resources/db/migration/V1__create_accounts_table.sql
+++ src/main/resources/db/migration/V1__create_accounts_table.sql
@@ -1,11 +1,11 @@
 CREATE TABLE account (
     id BIGINT NOT NULL AUTO_INCREMENT,
-    first_name varchar(255)       not null,
-    last_name varchar(255)        not null,
-    email varchar(255)            not null,
-    password varchar(255)         not null,
-    gender varchar(255)           not null,
-    created_at TIMESTAMP          not null,
-    updated_at TIMESTAMP          not null,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    gender VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
     PRIMARY KEY (id)
-)
+);
