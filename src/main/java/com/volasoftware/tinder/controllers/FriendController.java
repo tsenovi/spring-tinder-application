adaf71f6983c8e4a667963567b4338f7bcddb7ba
tsenovi <vandertsen@gmail.com>
2024-02-18 16:24:12 +0200

    Added new FriendController method rateFriend

--- src/main/java/com/volasoftware/tinder/controllers/FriendController.java
+++ src/main/java/com/volasoftware/tinder/controllers/FriendController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +26,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class FriendController {
 
     private final FriendService friendService;
+
+    private final RatingService ratingService;
 
     @GetMapping
     @ApiOperation(value = "Get sorted friends by location",
@@ -60,5 +63,22 @@ public class FriendController {
             AccountConstant.DETAILS,
             HttpStatus.OK,
             friendService.getFriendInfo(accountId));
+    }
+
+    @ApiOperation(value = "Rate friend")
+    @ApiResponses(
+        value = {
+            @ApiResponse(code = 200, message = "Success operation", response = FriendDto.class),
+            @ApiResponse(code = 401, message = "Unauthorized operation"),
+            @ApiResponse(code = 403, message = "Forbidden operation"),
+            @ApiResponse(code = 500, message = "Failed operation")
+        })
+    @PostMapping("/rate")
+    public ResponseEntity<?> rateFriend(
+        @RequestBody RatingDto ratingDto) {
+        return ResponseHandler.generateResponse(
+            RatingConstant.RATED_FRIEND,
+            HttpStatus.OK,
+            ratingService.rateFriend(ratingDto));
     }
 }
